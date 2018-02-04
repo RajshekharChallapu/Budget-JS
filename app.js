4e6340225ac9138ce2547c2e95f7985cd9b3ad9e
CR <rajshekhar@protonmail.com>
2018-02-03 19:22:36 -0600

    adding new item to controller to show budget in UI

--- app.js
+++ app.js
@@ -63,7 +63,9 @@ var DOMstrings={
   inputType:'.add__type',
   inputDescription: '.add__description',
   inputValue:'.add__value',
-  inputBtn:'.add__btn'
+  inputBtn:'.add__btn',
+  incomeContainer:'.income__list',
+  expensesContainer:'.expenses__list'
 };
 
   return {
@@ -71,8 +73,42 @@ var DOMstrings={
       return {
            type: document.querySelector(DOMstrings.inputType).value, // will inc either exp
            description: documnet.querySelector(DOMstrings.inputDescription).value,
-          value: document.querySelector(DOMstrings.inputValue).value
+           value: document.querySelector(DOMstrings.inputValue).value
           };
+        },
+
+        addListItem: function(obj, type){
+          var html, newHtml, element;
+          // Create HTML string with placeholder text
+
+          if(type === 'inc'){
+          element= DOMstrings.incomeContainer;
+           html = '<div class="item clearfix" id="income-%id%">\
+          <div class="item__description">%description%</div>\
+          <div class="right clearfix"><div class="item__value">%value%</div>\
+            <div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div>\
+          </div>\
+        </div>';
+
+        }else if(type ==='exp'){
+        element =DOMstrings.expensesContainer;
+          html='<div class="item clearfix" id="expense-%id%">\
+          <div class="item__description">%description%</div>\
+          <div class="right clearfix"><div class="item__value">%value%</div>\
+          <div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn">\
+          <i class="ion-ios-close-outline"></i></button></div>\
+          </div>\
+          </div>';
+        }
+
+          //Replace the placeholder text with some actual data
+          newHtml = html.replace('%id%', obj.id);
+          newHtml = newHtml.replace('%description%', obj.description);
+          newHtml = newHtml.replace('%value%', obj.value);
+
+          //Insert the HTML into DOM
+          documnet.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
+
         },
         getDOMstrings: function(){
           return DOMstrings;
@@ -94,7 +130,7 @@ var DOMstrings={
         });
       }
       var ctrlAddItem = function() {
-        var input, newItem;
+        var input,newItem;
 
         // get input data ..after click
         input = UICtrl.getInput();
@@ -103,6 +139,7 @@ var DOMstrings={
       newItem= budgetCtrl.addItem(input.type, input.description, input.value);
 
         //Add UI too..display on UI
+        UICtrl.addListItem(newItem, input.type);
 
       };
 return {
