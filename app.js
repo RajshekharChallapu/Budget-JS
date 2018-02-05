2c3d9dfb617a5f357f93ddf7634ab3ee520e6cbd
CR <rajshekhar@protonmail.com>
2018-02-04 20:29:24 -0600

    budget controller sum, percentage

--- app.js
+++ app.js
@@ -11,6 +11,16 @@ var budgetController = (function () {
         this.description = description;
         this.value = value;
     };
+
+    var calculateTotal = function (type){
+        var sum =0;
+        data.allItems[type].forEach(function(cur){
+          sum +=cur.value;
+
+        });
+        data.totals[type]= sum;
+
+    };
     //DATA Structures
     var data = {
         allItems: {
@@ -21,7 +31,9 @@ var budgetController = (function () {
         totals: {
             exp: 0,
             inc: 0
-        }
+        },
+        budget:0,
+        percentage:-1
 
     };
     return {
@@ -47,6 +59,31 @@ var budgetController = (function () {
 
             //Return the new element
             return newItem;
+        },
+
+        calculateBudget: function(){
+
+          //calculate total income and expenses
+          calculateTotal('exp');
+          calculateTotal('inc');
+
+          // calculate the budget: income expenses
+          data.budget = data.totals.inc -data.totals.exp;
+          //calculate the percentage o0f income thet we setupEventListeners
+          if(data.totals.inc> 0){
+          data.percentage =Math.round((data.totals.exp/ data.totals.inc) * 100);
+        }else{
+          data.percentage =-1;
+        }
+        },
+
+        getBudget: function(){
+          return{
+            budget: data.budget,
+            totalInc: data.totals.inc,
+            totalExp: data.totals.exp,
+            percentage: data.percentage
+          };
         },
 
         testing: function () {
@@ -144,10 +181,13 @@ var controller = (function (budgetCtrl,UICtrl) {
 
     var updateBudget = function(){
       // calculate the budget__title
+      budgetCtrl.calculateBudget();
+      // return the budget
+      var budget = budgetCtrl.getBudget();
 
-      // return the budget__title
 
       //display the budget on the UI
+      console.log(budget);
 
     };
     var ctrlAddItem = function () {
