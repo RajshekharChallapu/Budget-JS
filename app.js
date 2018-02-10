53eaaf1f0cb9128a9d22f6501ea91cafb503dc33
CR <rajshekhar@protonmail.com>
2018-02-09 23:27:57 -0600

    displaying current month and year

--- app.js
+++ app.js
@@ -150,7 +150,8 @@ var UIController = (function() {
     expensesLabel: '.budget__expenses--value',
     percentageLabel: '.budget__expenses--percentage',
     container: '.container',
-    expensesPercLabel:'.item__percentage'
+    expensesPercLabel:'.item__percentage',
+    dateLabel: '.budget__title--month'
   };
   var formatNumber = function(num, type){
 
@@ -227,8 +228,7 @@ var UIController = (function() {
 
     },
     clearFields: function() {
-      var fields,
-        fieldsArr;
+      var fields,fieldsArr;
 
       fields = document.querySelectorAll(DOMstrings.inputDescription + ',' + DOMstrings.inputValue);
 
@@ -276,6 +276,15 @@ var UIController = (function() {
 
       });
 
+    },
+    displayMonth: function(){
+      var now,months,month,year;
+      now =new Date();
+      months =['January', 'Feb', 'March','April','May','June','July','Aug','Sep','Oct','Nov','December']
+      month = now.getMonth();
+
+      year= now.getFullYear();
+      document.querySelector(DOMstrings.dateLabel).textContent = months[month] +' ' +year;
     },
 
     getDOMstrings: function() {
@@ -368,6 +377,7 @@ var controller = (function(budgetCtrl, UICtrl) {
   return {
     init: function() {
       console.log('Application has started.');
+      UICtrl.displayMonth();
       UICtrl.displayBudget({budget: 0, totalInc: 0, totalExp: 0, percentage: -1});
       setupEventListeners();
     }
